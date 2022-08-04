36cefb5b9ffaf044e163a786b9fcdea8f9ff9387
pavanrm2019 <pavanrm86@gmail.com>
2022-08-04 19:21:24 +0530

    second commit

--- src/manchester/testNocOne.java
+++ src/manchester/testNocOne.java
@@ -5,6 +5,7 @@ public class testNocOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello one");
+System.out.println("hello two");
 	}
 
 }
