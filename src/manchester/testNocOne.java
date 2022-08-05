d9ec661b767ab8b419c96c8717f7cc17e01bc66c
pavanrm2019 <pavanrm86@gmail.com>
2022-08-05 10:08:54 +0530

    hello to hi changes for homeonebranch second commit

--- src/manchester/testNocOne.java
+++ src/manchester/testNocOne.java
@@ -4,8 +4,8 @@ public class testNocOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello homeone one");
-System.out.println("hello homeone two");
+System.out.println("hi homeone one");
+System.out.println("hi homeone two");
 	}
 
 }
