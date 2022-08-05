01f813a8a7974aac965a2cdc8c3e515adf0f709c
pavanrm2019 <pavanrm86@gmail.com>
2022-08-05 09:10:38 +0530

    homeone branch first commit

--- src/manchester/testNocOne.java
+++ src/manchester/testNocOne.java
@@ -4,8 +4,8 @@ public class testNocOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello one");
-System.out.println("hello two");
+System.out.println("hello homeone one");
+System.out.println("hello homeone two");
 	}
 
 }
