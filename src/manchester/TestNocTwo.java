d5c90b7fed6d1d738d234ade56d584940a850a89
pavanrm2019 <pavanrm86@gmail.com>
2022-08-05 09:13:45 +0530

    hometwo first commit

---
+++ src/manchester/TestNocTwo.java
@@ -0,0 +1,11 @@
+package manchester;
+
+public class TestNocTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello hometwo  one");
+		System.out.println("hello hometwo two");
+	}
+
+}
